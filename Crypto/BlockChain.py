2b166cee3dbac8092ddb90c8b988c506e58bdec2
Vaibhav Kurhe <mcs192572@cse.iitd.ac.in>
2020-07-30 14:54:42 +0530

    Move mempool and unspntTxOut to BlockChain class; refactor the insert(block) method

--- Crypto/BlockChain.py
+++ Crypto/BlockChain.py
@@ -1,47 +1,66 @@
 from Block import Block
 from typing import Dict, List, Optional, Tuple
 from Transaction import Transaction, TransactionOutput
+import copy
 
 class BlockChain:
     def __init__(self) -> None:
+        self.mempool: Dict[str, Transaction] = {} # unconfirmed transactions
+        self.unspntTxOut: Dict[str, List[TransactionOutput]] = {} # confirmed but unspent transactions outputs
         self.headMap: Dict[str, BlockNode] = {} # map from a head block hash to the blocknode
-        self.headStates: Dict[str, Tuple[Dict[str, Transaction], Dict[str, TransactionOutput]]] = {}
         self.longest: str = "" # longest chain-head hash
         self.blockMap: Dict[str, BlockNode] = {} # map from all block hashes to the blocknode
+        # blockstates is a Dict[blockHash, (mempool, unspntTxOut)]
+        self.blockStates: Dict[str, Tuple[Dict[str, Transaction], Dict[str, List[TransactionOutput]]]] = {}
 
     # insert a new block into blockchain, return True (if all ok) return False (if block rejected)
-    def insert(self, block: Block, mempool: Dict[str, Transaction], unspntTxOut: Dict[str, TransactionOutput]) -> Tuple[bool, Dict, Dict]:
-        ret0 = {}
-        ret1 = {}
+    def insert(self, block: Block) -> bool:
+        # if this is Genesis Block
         if block.blockHeader.prevBlock is "":
-            # Genesis Block
             newBlkNode = BlockNode(block)
+            self.blockMap[block.hash] = newBlkNode
             self.headMap[block.hash] = newBlkNode
             self.longest = block.hash
-            self.headStates[block.hash] = (mempool, unspntTxOut)
+            unspntTxOut = {}
+            for txn in block.txnList:
+                unspntTxOut[txn.getHash()] = txn.txnOutputs
+            self.blockStates[block.hash] = ({}, unspntTxOut)
+            self.mempool = self.blockStates[self.longest][0]
+            self.unspntTxOut = self.blockStates[self.longest][1]
+        # for any block other than genesis block
         else:
-            # if prevblock is already one of the heads
-            if block.blockHeader.prevBlock in self.headMap:
-                newBlkNode = BlockNode(block, self.headMap[block.blockHeader.prevBlock])
-                self.blockMap[block.hash] = newBlkNode
-                self.headMap[block.hash] = newBlkNode
-                if self.longest == "" or self.headMap[self.longest].len < newBlkNode.len:
-                    self.headStates[self.longest] = (mempool, unspntTxOut)
-                    self.longest = block.hash
-                    ret0 = self.headStates[block.blockHeader.prevBlock][0]
-                    ret1 = self.headStates[block.blockHeader.prevBlock][1]
-                self.headMap.pop(block.blockHeader.prevBlock)
-                self.headStates.pop(block.blockHeader.prevBlock)
-            # if prevblock is not a head
-            elif block.blockHeader.prevBlock in self.blockMap:
+            # prevblock exists in the blockchain
+            if block.blockHeader.prevBlock in self.blockMap:
+                # check if any transaction in this new block is unknown to us
+                for txn in block.txnList:
+                    if txn.getHash() not in self.blockStates[block.blockHeader.prevBlock][0]:
+                        # invalid transaction; reject the block
+                        return False
+                # update the mempool and unspntTxOut structures for the new block
+                self.blockStates[block.hash] = (copy.deepcopy(self.blockStates[block.blockHeader.prevBlock][0]), copy.deepcopy(self.blockStates[block.blockHeader.prevBlock][1]))
+                for txn in block.txnList:
+                    if txn.getHash() in self.blockStates[block.hash][0]:
+                        self.blockStates[block.hash][1][txn.getHash()] = txn.txnOutputs
+                        self.blockStates[block.hash][0].pop(txn.getHash())
+                    else:
+                        # invalid path; impossible to come here
+                        return False
+                # add the new block to blockMap and headMap
                 newBlkNode = BlockNode(block, self.blockMap[block.blockHeader.prevBlock])
                 self.blockMap[block.hash] = newBlkNode
                 self.headMap[block.hash] = newBlkNode
-            # if prevblock doesn't exist in any map
+                # update the longest pointer and corresponding mempool and unspntTxout cache if necessary
+                if block.blockHeader.prevBlock in self.headMap:
+                    if newBlkNode.len > self.headMap[self.longest].len:
+                        self.longest = block.hash
+                        self.mempool = self.blockStates[self.longest][0]
+                        self.unspntTxOut = self.blockStates[self.longest][1]
+                    self.headMap.pop(block.blockHeader.prevBlock)
+            # prevblock absent in the blockchain
             else:
-                # reject / ignore this block
-                return (False, ret0, ret1)
-        return (True, ret0, ret1)
+                # invalid block; reject it
+                return False
+        return True
 
 # unit of node used inside blockchain implemented as linked list
 class BlockNode:
